a74c0925c379f8ce6903c611c28ace45972a0e83
akshayrathi2000 <47154936+akshayrathi2000@users.noreply.github.com>
2020-03-31 22:42:49 +0530

    Add files via upload

---
+++ catalog/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import *
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display=('book', 'id', 'status', 'borrower' , 'due_back')
+    list_filter=('status', 'due_back')
+    fieldsets=(
+    (None,{'fields':('book', 'imprint', 'id')}),
+    ('Availability',{'fields':('status','due_back','borrower')})
+    )
+
+class BooksInstanceInline(admin.TabularInline):
+    model=BookInstance
+    extra=0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display=('title','author','display_genre')
+    inlines=[BooksInstanceInline]
+
+class BooksInline(admin.TabularInline):
+    model=Book
+    extra=0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','date_of_birth','date_of_death')
+    fields=['first_name','last_name', ('date_of_birth', 'date_of_death')]
+    inlines=[BooksInline]
+
+admin.site.register(Author, AuthorAdmin)
